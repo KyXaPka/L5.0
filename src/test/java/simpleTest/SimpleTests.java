46232ae62438e70b5ec49f6188ca0ecd2cb874e0
KyXaPKa <Theprojectstufftv@gmail.com>
2020-06-02 09:11:51 +0400

    first commit

---
+++ src/test/java/simpleTest/SimpleTests.java
@@ -0,0 +1,12 @@
+package simpleTest;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+import org.testng.asserts.SoftAssert;
+
+public class SimpleTests {
+
+    @Test
+    public void SimpleTests() {
+    }
+}
